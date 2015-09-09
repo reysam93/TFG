6c184e2c67e91e122d7030f8f220dea21a749d13
Samuel <samuel.rey.escudero@gmail.com>
2015-09-09 22:06:59 +0200

    code generated from example3.xml

---
+++ examples/build/CMakeFiles/automata.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/automata.dir/example3.cpp.o"
+  "CMakeFiles/automata.dir/home/samuel/University/TFG/visualHFSM/automatagui.cpp.o"
+  "CMakeFiles/automata.dir/home/samuel/University/TFG/visualHFSM/point.cpp.o"
+  "CMakeFiles/automata.dir/home/samuel/University/TFG/visualHFSM/node.cpp.o"
+  "CMakeFiles/automata.dir/home/samuel/University/TFG/visualHFSM/transition.cpp.o"
+  "CMakeFiles/automata.dir/home/samuel/University/TFG/visualHFSM/subautomata.cpp.o"
+  "CMakeFiles/automata.dir/home/samuel/University/TFG/visualHFSM/guinode.cpp.o"
+  "CMakeFiles/automata.dir/home/samuel/University/TFG/visualHFSM/guitransition.cpp.o"
+  "CMakeFiles/automata.dir/home/samuel/University/TFG/visualHFSM/guisubautomata.cpp.o"
+  "CMakeFiles/automata.dir/home/samuel/University/TFG/visualHFSM/popups/editnodedialog.cpp.o"
+  "CMakeFiles/automata.dir/home/samuel/University/TFG/visualHFSM/popups/edittransitiondialog.cpp.o"
+  "CMakeFiles/automata.dir/home/samuel/University/TFG/visualHFSM/popups/edittransitioncodedialog.cpp.o"
+  "CMakeFiles/automata.dir/home/samuel/University/TFG/visualHFSM/popups/renamedialog.cpp.o"
+  "CMakeFiles/automata.dir/home/samuel/University/TFG/visualHFSM/popups/renametransitiondialog.cpp.o"
+  "../automata.pdb"
+  "../automata"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/automata.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
